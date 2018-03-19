5ff42eb44cf8bd7d19b114530cf7624594fa1b76
warddr <git@warddr.eu>
2018-03-19 20:01:57 +0100

    clean up tab and space

--- mqtt.php
+++ mqtt.php
@@ -3,9 +3,9 @@ include "config.php";
 
 function sendmessage($topic, $message, $qos=0) {
 	global $mqttuser,$mqttaddr,$mqttpass;
-    $client = new Mosquitto\Client;
-    $client->setCredentials($mqttuser, $mqttpass);
-    $client->connect($mqttaddr);
+	$client = new Mosquitto\Client;
+	$client->setCredentials($mqttuser, $mqttpass);
+	$client->connect($mqttaddr);
 	$client->publish($topic, $message, $qos);
 	$client->loop();
 	$client->disconnect();
