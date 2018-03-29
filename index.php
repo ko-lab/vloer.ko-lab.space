f12858d03cd6493eed3ecbc0a2fde56aa46a6e5f
warddr <git@warddr.eu>
2018-03-29 21:48:52 +0200

    add buttons

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ if($_POST['knop']=="strandtest") {
 	sendmessage("vloer/startscript", "strandtest");
 }
 
+if($_POST['knop']=="4or") {
+        sendmessage("vloer/startscript", "4or");
+}
+
+if($_POST['knop']=="matrix") {
+        sendmessage("vloer/startscript", "matrix");
+}
+
 if($_POST['knop']=="stop") {
 	sendmessage("vloer/startscript", "stop");
 }
@@ -15,7 +23,9 @@ if($_POST['knop']=="stop") {
 <form method="post" target="votar">
     <button name="knop" value="strandtest">strandtest</button>
     <button name="knop" value="stop">stop</button>
-</form> 
+    <button name="knop" value="4or">4 op een rij</button>
+    <button name="knop" value="matrix">matrix effect</button>
+</form>
 <br /><br />
 
 <a href="controller.php?id=1">Open controller player 1</a><br />
