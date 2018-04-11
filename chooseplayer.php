8f621aec9c5a9c4a907a44d8bc2ef164cbcf32eb
Andreas Busschop <andreas.experiment@live.be>
2018-04-11 23:07:47 +0200

    New look of website with better UI

---
+++ chooseplayer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="menu.css">
+</head>
+<body>
+<br></br> <br></br> <p>  <a href="controller.php?id=1" class="p1">speler 1</a></p> <br></br> <p><a href="controller.php?id=2" class="p2">speler 2</a></p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="menu.css">
+<script>
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function opEenRij4(){
+
+    var content = 	 " <br></br> <br></br> <p>  <a href=\"controller.php?id=1\" class=\"p1\">speler 1</a></p> <br></br> <p><a href=\"controller.php?id=1\" class=\"p2\">speler 2</a></p>";
+    document.getElementsByTagName('body')[0].innerHTML = content;
+
+	}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
+</head>
+<body>
 <?php
 require_once "mqtt.php";
 
@@ -7,6 +42,7 @@ if($_POST['knop']=="strandtest") {
 
 if($_POST['knop']=="4or") {
         sendmessage("vloer/startscript", "4or");
+	echo "<script>window.location = 'https://vloer.ko-lab.space/chooseplayer.php'</script>";
 }
 
 if($_POST['knop']=="matrix") {
@@ -18,17 +54,19 @@ if($_POST['knop']=="stop") {
 }
 ?>
 
+<h1>Welkom op de ko-lab ledvloer</h1>
+<p>Klik op de knop om te selecteren wat u wilt doen met onze super coole vloer!</p>
 
-<iframe name="votar" style="display:none;"></iframe>
-<form method="post" target="votar">
-    <button name="knop" value="strandtest">strandtest</button>
-    <button name="knop" value="stop">stop</button>
-    <button name="knop" value="4or">4 op een rij</button>
-    <button name="knop" value="matrix">matrix effect</button>
+<div class="dropdown">
+<form method="post" target="#">
+	<button type="button" onclick="myFunction()" class="dropbtn">klik op mij</button>
+	  <div id="myDropdown" class="dropdown-content">
+	     <button name="knop" value="strandtest" class="vanbinnen">strandtest</button>
+	    <button name="knop" value="stop" class="vanbinnen">stop</button>
+	    <button name="knop" value="4or" class="vanbinnen">4 op een rij</button>
+		<button name="knop" value="matrix" class="vanbinnen">matrix effect</button>
+	  </div>
 </form>
-<br /><br />
-
-<a href="controller.php?id=1">Open controller player 1</a><br />
-<a href="controller.php?id=2">Open controller player 2</a>
-
-
+</div>
+</body>
+</html>
